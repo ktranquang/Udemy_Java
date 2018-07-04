ce85ac7921c441607f7334dfe95976665cfa6924
Khoi Tran-Quang <ktranquang@hotmail.com>
2018-07-03 20:34:07 -0400

    Update

--- Operators/src/com/khoi/Main.java
+++ Operators/src/com/khoi/Main.java
@@ -65,8 +65,26 @@ public class Main {
         if(isCar == true)
             System.out.println("This is not supposed to happen");
 
+        isCar = true;
         boolean wasCar = isCar ? true : false;
         if (wasCar)
             System.out.println("wasCar is true");
+
+        // 1. Create a double variable wtih the value 20
+        // 2. Create a second variable of type double with the value 80
+        // 3. Add both numbers up and multiply by 25
+        // 4. Use the reminder operator to figure out the reminder from the sum of #3 divided by 40
+        // 5. Write an "if" statement that displays a message "Total was over the limit"
+        //    if the remaining total (#4) is equal to 20 or less.
+
+        double myFirstValue = 20d;
+        double mySecondValue = 80;
+        double myTotal = (myFirstValue + mySecondValue) * 25;
+        System.out.println("myTotal = " + myTotal);
+        double theRemainder = myTotal % 40;
+        System.out.println("Remainder is " + theRemainder);
+        if(theRemainder <= 20)
+            System.out.println("Total was over the limit");
+
     }
 }
