790fd05bf23178f3c6114e2a236d77d27ccfc0d3
Khoi Tran-Quang <ktranquang@hotmail.com>
2018-08-10 16:20:15 -0400

    Update

--- Methods/src/com/khoi/Main.java
+++ Methods/src/com/khoi/Main.java
@@ -28,9 +28,9 @@ public class Main {
         // it should be sent one argument only, the player score
         // it should return an int
         // the return data should be
-        // 1 if the score is >1000
-        // 2 if the score is >500 and < 1000
-        // 3 if the score is > 100 and < 500
+        // 1 if the score is >=1000
+        // 2 if the score is >=500 and < 1000
+        // 3 if the score is >=100 and < 500
         // 4 in all other cases
         // call both methods and display the results of the following
         // a score of 1500, 900, 400, and 50
@@ -51,11 +51,11 @@ public class Main {
 
     public static int calculateHighScorePosition(int playerScore) { // method AKA function
 
-        if (playerScore > 1000){
+        if (playerScore >= 1000){
             return 1;
-        } else if (playerScore > 500 && playerScore < 1000) {
+        } else if (playerScore >= 500 && playerScore < 1000) {
             return 2;
-        } else if (playerScore > 100 && playerScore < 500) {
+        } else if (playerScore >= 100 && playerScore < 500) {
             return 3;
         } else {
             return 4;
