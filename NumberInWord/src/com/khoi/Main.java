d90f19bc361a46eef3187e669f1124092567f108
Khoi Tran-Quang <ktranquang@hotmail.com>
2019-01-16 20:23:22 -0500

    "Update (9%)"

---
+++ NumberInWord/src/com/khoi/Main.java
@@ -0,0 +1,11 @@
+package com.khoi;
+
+/**
+ * Created by khoi on Jan, 2019
+ */
+public class Main {
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(10);
+    }
+}
+
