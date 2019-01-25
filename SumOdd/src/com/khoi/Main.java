bea32f77c8399833d5afee80f510513bd20a3e6d
Khoi Tran-Quang <ktranquang@hotmail.com>
2019-01-25 13:50:19 -0500

    "Update (10%)"

---
+++ SumOdd/src/com/khoi/Main.java
@@ -0,0 +1,17 @@
+package com.khoi;
+
+/**
+ * Created by khoi on Jan, 2019
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SumOddRange.sumOdd(1, 100));
+        System.out.println(SumOddRange.sumOdd(-1, 100));
+        System.out.println(SumOddRange.sumOdd(100, 100));
+        System.out.println(SumOddRange.sumOdd(100, -100));
+        System.out.println(SumOddRange.sumOdd(100, 1000));
+
+    }
+
+
+}
