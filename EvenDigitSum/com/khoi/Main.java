d6218388752fee9a4b6bf6d0e6255e876801c2f1
tranquk <tranquk@Z500W21533.le500.loto-quebec.com>
2019-03-21 11:13:17 -0400

    Update

--- EvenDigitSum/com/khoi/Main.java
+++ EvenDigitSum/com/khoi/Main.java
@@ -2,7 +2,6 @@ package com.khoi;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hi");
 		System.out.println(getEvenDigitSum(123456789));
 		System.out.println(getEvenDigitSum(252));
 		System.out.println(getEvenDigitSum(-22));
@@ -15,9 +14,9 @@ public class Main {
         } 
         while (number > 0) {
             if ((number % 2) == 0) {
-                sum += number%10;
-                number /= 10;
-        }
+                sum += number % 10;
+                }
+            number /= 10;
     } return sum;
 }
 
